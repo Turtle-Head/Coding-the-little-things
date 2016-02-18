7c29d673592ca92a72198d8e2417c036af0a5233
James Fehr <fehr.james@gmail.com>
2016-02-17 23:53:57 -0600

    Multiple updates
    --Banner changes
    --Nav Bar changes
    --Title moved to Nav Bar
    --Added several classes
    --Added Icon for site
    --Started the Google API Post

--- js/blogger.js
+++ js/blogger.js
@@ -3,21 +3,27 @@
 
 $(function(){
 
-	var loader = function( filename ) {
+	var loader = function( filename, title ) {
 		jQuery.get(filename, function(data) {
    			$('#article').html(data);
-			$('.content').hide();
-			$('#article').show(true);
+				$('.content').hide();
+				$('#loc').html('<h2>' + title + '</h2><hr width="50%" color="white">');
+				$('#article').show(true);
 		});
 
 	};
 
 	var Article = [
-    	{
+    {
 			'id': '1',
 			'title': 'About Me',
 			'articleSrc': 'assets/aboutMe.html',
-			'date': 'Some date'
+			'date': '2016-2-10 20:42:23'
+		},{
+			'id': '2',
+			'title': 'Getting Started With Google Maps',
+			'articleSrc': 'assets/googleAPI.html',
+			'date': '2016-2-17 16:30:00'
 		}
 	];
 
@@ -34,11 +40,12 @@ $(function(){
 	$(".linky").on("click", function(){
 		if(this.id === 'home') {
 			$('.content').hide();
+			$('#loc').html('<h2>Coding the little things...</h2><hr width="50%">');
 			$('#welcome').show(true);
 		} else {
 			for(var i = 0; i < Article.length; i++) {
 				if (Number(Article[i].id) === Number(this.id)) {
-					loader(Article[i].articleSrc);
+					loader(Article[i].articleSrc, Article[i].title);
 				} else {
 					$('#article').text('File not found !!!');
 					$('.content').hide();
