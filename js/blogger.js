0b6c4ed997ba5ff2db2274688cf453efcc9ffc99
James Fehr <Turtle-Head@users.noreply.github.com>
2016-10-01 16:53:35 -0700

    Added changed JS

--- js/blogger.js
+++ js/blogger.js
@@ -9,38 +9,33 @@ $(function(){
 				$('.content').hide();
 				$('#loc').html('<h2>' + title + '</h2><hr width="50%" color="white">');
 				$('#article').show(true);
-			});
+		});
+
 	};
 
 	var Article = [
-		{
-			'id': '3',
-			'title': 'Portfolio',
-			'articleSrc': 'http://turtle-head.github.io/Portfolio',
-			'local': false
-		},
-		{
-			'id': '2',
-			'title': 'A Simple Map',
-			'articleSrc': 'assets/googleAPI.html',
-			'local': true
-		},{
+    {
 			'id': '1',
 			'title': 'About Me',
 			'articleSrc': 'assets/aboutMe.html',
-			'local': true
+			'date': '2016-2-10 20:42:23'
+		},{
+			'id': '2',
+			'title': 'Getting Started With Google Maps',
+			'articleSrc': 'assets/googleAPI.html',
+			'date': '2016-2-17 16:30:00'
+		},{
+			'id': '3',
+			'title': 'Thing I think about',
+			'articleSrc': 'assets/things.html',
+			'date': '2016-7-28'
 		}
 	];
 
 	var View = function() {
 		var output;
 		for ( var x = 0; x < Article.length; x++) {
-			if (Article[x].local){
-				output = '<a href="#" id=' + Article[x].id + ' class="linky">' + Article[x].title + '</a>';
-			}
-			else if (!Article[x].local) {
-				output = '<a href=' + Article[x].articleSrc + ' target="_blank" class="linky" id="nl">' + Article[x].title + '</a>';
-			}
+			output = '<a href="#" id=' + Article[x].id + ' class="linky">' + Article[x].title + '</a>';
 			$('nav').append( output );
 		}
 	};
@@ -52,9 +47,7 @@ $(function(){
 			$('.content').hide();
 			$('#loc').html('<h2>Coding the little things...</h2><hr width="50%">');
 			$('#welcome').show(true);
-		} else if(this.id === 'nl') {
-			// Outside site reference...redirected
-		}	else {
+		} else {
 			for(var i = 0; i < Article.length; i++) {
 				if (Number(Article[i].id) === Number(this.id)) {
 					loader(Article[i].articleSrc, Article[i].title);
