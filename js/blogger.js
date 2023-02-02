f1bfdbd4e3a2873432d679025feba38380febe53
James Fehr <fehr.james@gmail.com>
2023-02-02 00:24:58 -0800

    updates

--- js/blogger.js
+++ js/blogger.js
@@ -1,77 +1,82 @@
 // js/blogger.js
 // 2016 James Fehr
 
-$(function(){
+$(function () {
+  var loader = function (filename, title) {
+    jQuery.get(filename, function (data) {
+      $('#article').html(data)
+      $('.content').hide()
+      $('#loc').html('<h2>' + title + '</h2><hr width="50%" color="white">')
+      $('#article').show(true)
+    })
+  }
 
-	var loader = function( filename, title ) {
-		jQuery.get(filename, function(data) {
-   			$('#article').html(data);
-				$('.content').hide();
-				$('#loc').html('<h2>' + title + '</h2><hr width="50%" color="white">');
-				$('#article').show(true);
-		});
-
-	};
-
-	var Article = [
+  var Article = [
     {
-			'id': '1',
-			'title': 'About Me',
-			'articleSrc': 'assets/aboutMe.html',
-			'date': '2016-2-10 20:42:23'
-		},{
-			'id': '2',
-			'title': 'Getting Started With Google Maps',
-			'articleSrc': 'assets/googleAPI.html',
-			'date': '2016-2-17 16:30:00'
-		},{
-			'id': '3',
-			'title': 'Thing I think about',
-			'articleSrc': 'assets/things.html',
-			'date': '2016-7-28'
-		}
-	];
+      id: '1',
+      title: 'AboutMe',
+      articleSrc: 'assets/aboutMe.html',
+      date: '2016-2-10 20:42:23'
+    },
+    {
+      id: '2',
+      title: 'GoogleMaps',
+      articleSrc: 'assets/googleAPI.html',
+      date: '2016-2-17 16:30:00'
+    },
+    {
+      id: '3',
+      title: 'Thoughts',
+      articleSrc: 'assets/things.html',
+      date: '2016-7-28'
+    }
+  ]
 
-	var View = function() {
-		var output;
-		for ( var x = 0; x < Article.length; x++) {
-			output = '<a href="#" id=' + Article[x].id + ' class="linky">' + Article[x].title + '</a>';
-			$('nav').append( output );
-		}
-	};
+  var View = function () {
+    var output
+    for (var x = 0; x < Article.length; x++) {
+      output =
+        '<a href="#" id=' +
+        Article[x].id +
+        ' class="linky">' +
+        Article[x].title +
+        '</a>'
+      $('nav').append(output)
+    }
+  }
 
-	View();
+  View()
 
-	$(".linky").on("click", function(){
-		if(this.id === 'home') {
-			$('.content').hide();
-			$('#loc').html('<h2>Coding the little things...</h2><hr width="50%">');
-			$('#welcome').show(true);
-		} else {
-			for(var i = 0; i < Article.length; i++) {
-				if (Number(Article[i].id) === Number(this.id)) {
-					loader(Article[i].articleSrc, Article[i].title);
-				} else {
-					$('#article').text('File not found !!!');
-					$('.content').hide();
-					$('#article').show(true);
-				}
-			}
-		}
-	});
+  $('.linky').on('click', function () {
+    if (this.id === 'home') {
+      $('.content').hide()
+      $('#loc').html('<h2>Coding the little things...</h2><hr width="50%">')
+      $('#welcome').show(true)
+    } else {
+      for (var i = 0; i < Article.length; i++) {
+        if (Number(Article[i].id) === Number(this.id)) {
+          loader(Article[i].articleSrc, Article[i].title)
+        } else {
+          $('#article').text('File not found !!!')
+          $('.content').hide()
+          $('#article').show(true)
+        }
+      }
+    }
+  })
 
-	var stickyNavTop = $('nav').offset().top;
-	var stickyNav = function(){
-		var scrollTop = $(window).scrollTop();
-		if (scrollTop > stickyNavTop) {
-			$('nav').addClass('sticky');
-		} else {
-			$('nav').removeClass('sticky');
-		}
-	};
-	stickyNav();
-	$(window).scroll(function() {
-		stickyNav();
-	});
-	$('#article').hide();
-});
+  var stickyNavTop = $('nav').offset().top
+  var stickyNav = function () {
+    var scrollTop = $(window).scrollTop()
+    if (scrollTop > stickyNavTop) {
+      $('nav').addClass('sticky')
+    } else {
+      $('nav').removeClass('sticky')
+    }
+  }
+  stickyNav()
+  $(window).scroll(function () {
+    stickyNav()
+  })
+  $('#article').hide()
+})
